cc9b0d6b02aa9ede256827e7a00b2b31ef8df48e
longh <lelouchhe@gmail.com>
2023-04-17 15:57:05 -0700

    Update images

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 A template for Kodi addon
 
+## Run
+
+1. Enter addon browser
+2. Choose instal from zip
+3. Select the zip
+
+## Fail to install
+
+1. Uninstall the previous addon from My add-ons
+2. Restart kodi
+
 ## Resouce
 
 [wiki](https://kodi.wiki/view/Add-on_development)
--- addon.py
+++ addon.py
@@ -6,8 +6,9 @@ addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = 'http://localhost/some_video.mkv'
-li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+url = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+li = xbmcgui.ListItem('My First Video!')
+li.setProperty("IsPlayable", "true")
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
